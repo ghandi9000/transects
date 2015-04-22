6d1e5623334f7c665e449d43e0cbee66f27de7b0
Noah Peart <noah.v.peart@gmail.com>
2015-04-22 13:28:27 -0400

    Batch update

--- config.bat
+++ config.bat
@@ -1,7 +1,6 @@
 rem Data configuration/package setup
 @echo off
 for /f "tokens=*" %%G IN ('dir /B /A:D "C:\Program Files\R"') DO (
-    ECHO Using %%G
     set rdir="C:\Program Files\R\%%G\bin"
 )
 
@@ -10,8 +9,5 @@ ECHO adding %rdir% temporarily to PATH
 
 cd ./explore
 Rscript config.R
-
-rem cd ../models
-rem Rscript config.R
 
 pause
--- explore/remake.R
+++ explore/remake.R
@@ -3,7 +3,8 @@
 ## Description: Remake datasets for analysis
 ## Author: Noah Peart
 ## Created: Mon Apr 13 20:07:47 2015 (-0400)
-## Last-Updated: Wed Apr 22 02:23:23 2015 (-0400)
+R
+## Last-Updated: Wed Apr 22 13:27:01 2015 (-0400)
 ##           By: Noah Peart
 ######################################################################
 require(plyr)
@@ -24,21 +25,26 @@ if (file.exists("temp/transect.csv")) {
 ################################################################################
 ## tidy, wide -> long
 yrs <- c(87, 98, 99, 10, 11)
-cols <- grep("canht|^STAT|^DBH|^HT[[:digit:]]|^ht[[:digit:]]+|^bv|TRAN|TPLOT|TAG|SPEC|ASP|ELEV|DIST|^HR$", names(tp))
+cols <- grep("canht|^STAT|^DBH|^HT[[:digit:]]|^ht[[:digit:]]+|^bv|TRAN|TPLOT|TAG|SPEC|ASP|ELEV|DIST|^HR$|TRAD|ABSRAD", names(tp))
 dat <- tp[, cols]
 cols <- grep("[A-Za-z]+$|87$|98$|99$|10$|11$", names(dat))
 dat <- dat[, cols]  # remove other year columns
 dat[, paste0("HT", c(87, 98, 10))] <- NA
+names(dat)[names(dat) %in% c("ABSRAD", "TRAD")] <- c("ABSRAD99", "TRAD99")
+dat[, paste0("ABSRAD", c(87, 98, 10))] <- NA
+dat[, paste0("TRAD", c(87, 98, 10))] <- NA
 
 dat <- reshape(dat, times = yrs, direction = "long",
                varying = list(
+               TRAD = grep("^TRAD", names(dat)),
+               ABSRAD = grep("^ABSRAD", names(dat)),
                STAT = grep("^STAT", names(dat)),
                DBH = grep("^DBH", names(dat)),
                BV = grep("^bv", names(dat)),
                HT = grep("^ht[[:digit:]]", names(dat)),
                HTOBS = grep("^HT[[:digit:]]", names(dat)),
                CANHT = grep("canht", names(dat))),
-               v.names = c("STAT", "DBH", "BV", "HT", "HTOBS", "CANHT"),
+               v.names = c("TRAD","ABSRAD","STAT", "DBH", "BV", "HT", "HTOBS", "CANHT"),
                timevar = "YEAR")
 dat$YEAR <- factor(dat$YEAR)
 tp <- dat[!is.na(dat$DBH), ]
