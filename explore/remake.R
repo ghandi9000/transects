89544c3e475929524cddf1e5577f63b68c29ef7b
Noah Peart <noah.v.peart@gmail.com>
2015-04-24 11:12:15 -0400

    Batch update

--- explore/remake.R
+++ explore/remake.R
@@ -4,7 +4,7 @@
 ## Author: Noah Peart
 ## Created: Mon Apr 13 20:07:47 2015 (-0400)
 R
-## Last-Updated: Wed Apr 22 15:50:57 2015 (-0400)
+## Last-Updated: Fri Apr 24 11:11:38 2015 (-0400)
 ##           By: Noah Peart
 ######################################################################
 require(plyr)
@@ -55,3 +55,20 @@ saveRDS(tp, "../temp/tp.rds")
 ## inds <- match(interaction(dat$TPLOT, dat$TRAN, dat$YEAR),
 ##               interaction(hh_plot$TPLOT, hh_plot$TRAN, hh_plot$time))
 ## dat$CANHT <- hh_plot[inds, "ht_mean"]
+
+## Transect/tplot htobs counts (HH only)
+tst <- tp %>% group_by(TRAN, TPLOT) %>% filter(!is.na(HT99) | !is.na(HT11)) %>%
+    summarise(n99 = sum(!is.na(HT99)),
+              n11 = sum(!is.na(HT11)))
+
+tst2 <- dat %>% group_by(TRAN, TPLOT, YEAR) %>% filter(!is.na(HTOBS)) %>%
+    summarise(count = n())
+
+par(mfrow = c(2,1))
+plot(tp[tp$TRAN=="E320" & tp$TPLOT==16, "HT99"])
+plot(dat[dat$YEAR==87 & dat$TRAN=="E320" & dat$TPLOT==16, "HTOBS"])
+
+dd <- data.frame(v3=rep(3, 10), v2=rep(2, 10), v1=rep(1, 10), tag=sample(1:100, 10))
+reshape(dd, times=1:3, varying="v", v.names="v", direction = "long")
+sort(names(tp))
+tp <- tp[, order(names(tp))]
